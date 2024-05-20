f5bc55a3c1c6a5ac60dc97f56b32feb54d8ff6e3
JuaniGianfelice <juani.gianfelice@gmail.com>
2024-05-20 13:50:50 -0300

    feat(federico branch merge)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*NAV-BAR*/
+
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
@@ -6,3 +8,37 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolling');
     }
 });
+
+/*CARROUSELL*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselSlide = document.querySelector(".carousel-slide");
+    const carouselImages = document.querySelectorAll(".carousel-image");
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+    let currentIndex = 0;
+  
+    
+    function showImage(index) {
+      const slideWidth = carouselImages[0].clientWidth;
+      carouselSlide.style.transform = `translateX(-${index * slideWidth}px)`;
+      currentIndex = index;
+    }
+  
+    
+    showImage(currentIndex);
+  
+   
+    prevButton.addEventListener("click", function () {
+      currentIndex =
+        currentIndex > 0 ? currentIndex - 1 : carouselImages.length - 1;
+      showImage(currentIndex);
+    });
+  
+    
+    nextButton.addEventListener("click", function () {
+      currentIndex =
+        currentIndex < carouselImages.length - 1 ? currentIndex + 1 : 0;
+      showImage(currentIndex);
+    });
+  });
