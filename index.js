1713bae1084f28f3dc8315310d995b31d3d924ed
Matías Neira <matiasneirab@gmail.com>
2024-05-20 21:22:10 -0300

    add name in navbar

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 /*NAV-BAR*/
 
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 0) {
-        navbar.classList.add('scrolling');
-    } else {
-        navbar.classList.remove('scrolling');
-    }
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolling');
+  } else {
+    navbar.classList.remove('scrolling');
+  }
 });
 
 /*CARROUSELL*/
 
 document.addEventListener("DOMContentLoaded", function () {
-    const carouselSlide = document.querySelector(".carousel-slide");
-    const carouselImages = document.querySelectorAll(".carousel-image");
-    const prevButton = document.getElementById("prevButton");
-    const nextButton = document.getElementById("nextButton");
-    let currentIndex = 0;
-  
-    
-    function showImage(index) {
-      const slideWidth = carouselImages[0].clientWidth;
-      carouselSlide.style.transform = `translateX(-${index * slideWidth}px)`;
-      currentIndex = index;
-    }
-  
-    
+  const carouselSlide = document.querySelector(".carousel-slide");
+  const carouselImages = document.querySelectorAll(".carousel-image");
+  const prevButton = document.getElementById("prevButton");
+  const nextButton = document.getElementById("nextButton");
+  let currentIndex = 0;
+
+
+  function showImage(index) {
+    const slideWidth = carouselImages[0].clientWidth;
+    carouselSlide.style.transform = `translateX(-${index * slideWidth}px)`;
+    currentIndex = index;
+  }
+
+
+  showImage(currentIndex);
+
+
+  prevButton.addEventListener("click", function () {
+    currentIndex =
+      currentIndex > 0 ? currentIndex - 1 : carouselImages.length - 1;
     showImage(currentIndex);
-  
-   
-    prevButton.addEventListener("click", function () {
-      currentIndex =
-        currentIndex > 0 ? currentIndex - 1 : carouselImages.length - 1;
-      showImage(currentIndex);
-    });
-  
-    
-    nextButton.addEventListener("click", function () {
-      currentIndex =
-        currentIndex < carouselImages.length - 1 ? currentIndex + 1 : 0;
-      showImage(currentIndex);
-    });
   });
+
+
+  nextButton.addEventListener("click", function () {
+    currentIndex =
+      currentIndex < carouselImages.length - 1 ? currentIndex + 1 : 0;
+    showImage(currentIndex);
+  });
+});
+
+function nameUser() {
+  let userLS = JSON.parse(localStorage.getItem('users'));
+  let nameLS = userLS.name;
+  let iconUser = document.getElementById('iconUser');
+  if ( iconUser) {
+    iconUser.style.display = 'none';
+  let nameUser = document.getElementById('nameUser');
+    nameUser.textContent = nameLS;
+  } 
+}
