b484f74c6e9cd84090bc29496764d7ca29867967
JuaniGianfelice <juani_g_98@hotmail.com>
2024-05-21 01:39:31 -0300

    feat(img, about, products, footer, index)styles and html

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ window.addEventListener('scroll', function () {
 /*CARROUSELL*/
 
 document.addEventListener("DOMContentLoaded", function () {
-  const carouselSlide = document.querySelector(".carousel-slide");
-  const carouselImages = document.querySelectorAll(".carousel-image");
+  const carrouselSlide = document.querySelector(".carrousel-slide");
+  const carrouselImages = document.querySelectorAll(".carrousel-image");
   const prevButton = document.getElementById("prevButton");
   const nextButton = document.getElementById("nextButton");
   let currentIndex = 0;
 
 
   function showImage(index) {
-    const slideWidth = carouselImages[0].clientWidth;
-    carouselSlide.style.transform = `translateX(-${index * slideWidth}px)`;
+    const slideWidth = carrouselImages[0].clientWidth;
+    carrouselSlide.style.transform = `translateX(-${index * slideWidth}px)`;
     currentIndex = index;
   }
 
@@ -31,17 +31,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   prevButton.addEventListener("click", function () {
     currentIndex =
-      currentIndex > 0 ? currentIndex - 1 : carouselImages.length - 1;
+      currentIndex > 0 ? currentIndex - 1 : carrouselImages.length - 1;
     showImage(currentIndex);
   });
 
 
   nextButton.addEventListener("click", function () {
     currentIndex =
-      currentIndex < carouselImages.length - 1 ? currentIndex + 1 : 0;
+      currentIndex < carrouselImages.length - 1 ? currentIndex + 1 : 0;
     showImage(currentIndex);
   });
 });
+
+/*USER NAME*/
 
 function nameUser() {
   let userLS = JSON.parse(localStorage.getItem('users'));
